e218f0e802159fbdeaba29967c0209d34f84a9b3
TechBug2012 <mattbstanciu@gmail.com>
2017-02-19 03:29:47 -0600

    moved all into one class because all classes needed to extend JavaPlugin

--- src/net/extrillius/CodeDay/CodeDay.java
+++ src/net/extrillius/CodeDay/CodeDay.java
@@ -2,22 +2,32 @@ package net.extrillius.CodeDay;
 
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Created by mattbstanciu on 2/18/17.
  */
-public class CodeDay extends JavaPlugin {
+public class CodeDay extends JavaPlugin implements Listener {
 
     public void onEnable() {
-        this.getConfig().addDefault("checkpoints." + "", "");
+        this.getConfig().addDefault("checkpoints", "");
         this.getConfig().set("checkpoints." + "1.X", 0.0); //gold
         this.getConfig().set("checkpoints." + "1.Y", 0.0);
         this.getConfig().set("checkpoints." + "1.Z", 0.0);
@@ -41,12 +51,167 @@ public class CodeDay extends JavaPlugin {
             logger.log(Level.WARNING, "Don't forget to set the beginning loc");
         }
 
-        getServer().getPluginManager().registerEvents(new Events(), this);
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    //events
+    public Map<String, String> checkpoint = new HashMap<>();
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent e) {
+        Player p = e.getPlayer();
+        Inventory i = p.getInventory();
+
+        if (p.getLocation().getBlock().getType() == Material.GOLD_BLOCK) {
+
+            getConfig().set("checkpoints.1", p.getLocation()); //iffy
+            checkpoint.put(p.getName(), "1");
+
+            ItemStack goldSword = new ItemStack(Material.GOLD_SWORD);
+            ItemStack[] cp1 = {
+                    new ItemStack(Material.LEATHER_HELMET),
+                    new ItemStack(Material.LEATHER_CHESTPLATE),
+                    new ItemStack(Material.LEATHER_LEGGINGS) ,
+                    new ItemStack(Material.LEATHER_BOOTS)
+            };
+            cp1[0].setItemMeta(cp1[0].getItemMeta());
+            cp1[1].setItemMeta(cp1[1].getItemMeta());
+            cp1[2].setItemMeta(cp1[2].getItemMeta());
+            cp1[3].setItemMeta(cp1[3].getItemMeta());
+            goldSword.setItemMeta(goldSword.getItemMeta());
+            goldSword.setDurability((short) - 1);
+
+            i.clear();
+            i.setItem(1, goldSword);
+            p.getInventory().setArmorContents(cp1);
+        }
+        if (p.getLocation().getBlock().getType() == Material.IRON_BLOCK) {
+            getConfig().get("checkpoints.2", p.getLocation());
+            checkpoint.put(p.getName(), "2");
+
+            ItemStack ironSword = new ItemStack(Material.IRON_SWORD);
+            ItemStack[] cp2 = {
+                    new ItemStack(Material.IRON_HELMET),
+                    new ItemStack(Material.IRON_CHESTPLATE),
+                    new ItemStack(Material.IRON_LEGGINGS),
+                    new ItemStack(Material.IRON_BOOTS)
+            };
+            cp2[0].setItemMeta(cp2[0].getItemMeta());
+            cp2[1].setItemMeta(cp2[1].getItemMeta());
+            cp2[2].setItemMeta(cp2[2].getItemMeta());
+            cp2[3].setItemMeta(cp2[3].getItemMeta());
+            ironSword.setItemMeta(ironSword.getItemMeta());
+            ironSword.setDurability((short) - 1);
+
+            i.clear();
+            i.setItem(1, ironSword);
+            p.getInventory().setArmorContents(cp2);
+        }
+        if (p.getLocation().getBlock().getType() == Material.DIAMOND_BLOCK) {
+            getConfig().get("checkpoints.3", p.getLocation());
+            checkpoint.put(p.getName(), "3");
+
+            ItemStack diamondSword = new ItemStack(Material.IRON_SWORD);
+            ItemStack[] cp3 = {
+                    new ItemStack(Material.DIAMOND_HELMET),
+                    new ItemStack(Material.DIAMOND_CHESTPLATE),
+                    new ItemStack(Material.DIAMOND_LEGGINGS),
+                    new ItemStack(Material.DIAMOND_BOOTS)
+            };
+            cp3[0].setItemMeta(cp3[0].getItemMeta());
+            cp3[1].setItemMeta(cp3[1].getItemMeta());
+            cp3[2].setItemMeta(cp3[2].getItemMeta());
+            cp3[3].setItemMeta(cp3[3].getItemMeta());
+            diamondSword.setItemMeta(diamondSword.getItemMeta());
+            diamondSword.setDurability((short) - 1);
+
+            i.clear();
+            i.setItem(1, diamondSword);
+            p.getInventory().setArmorContents(cp3);
+        }
+        if (p.getLocation().getBlock().getType() == Material.OBSIDIAN) {
+            getConfig().get("checkpoints.end", p.getLocation());
+            checkpoint.put(p.getName(), "end");
+
+            i.clear();
+            for (Player q : getServer().getOnlinePlayers()) {
+                q.teleport(new Location(p.getWorld(), getConfig().getDouble("checkpoints.beginning.X"),
+                        getConfig().getDouble("checkpoints.beginning.Y"),
+                        getConfig().getDouble("checkpoints.beginning.Z")));
+            }
+            getServer().broadcastMessage(ChatColor.DARK_RED + p.getName() + " WON THE GAME!");
+            clear();
+        }
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        final Player p = e.getEntity();
+
+        getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+            @Override
+            public void run() {
+                if (p.isDead()) {
+                    p.setMaxHealth(20);
+                    p.setHealth(20);
+                }
+            }
+        });
+        p.teleport(new Location(p.getWorld(), getConfig().getDouble("checkpoints." + checkpoint.get(p.getName()) + ".X"),
+                getConfig().getDouble("checkpoints." + checkpoint.get(p.getName()) + ".Y"),
+                getConfig().getDouble("checkpoints." + checkpoint.get(p.getName()) + ".Z")));
+    }
+    //queue
+    private ArrayList<Player> queue = new ArrayList<>();
+    public static int seconds = 10;
+    int timer;
+
+    public void addToQueue(Player p) {
+        if (queue.contains(p)) {
+            p.sendMessage(ChatColor.RED + "You are already in the queue!");
+        }
+        else {
+            queue.add(p);
+            p.sendMessage(ChatColor.GREEN + "You have been added to the queue");
+        }
+    }
+    public void teleportPlayers() {
+        for (Player p : getServer().getOnlinePlayers()) {
+            queue.remove(p);
+            p.teleport(new Location(p.getWorld(), getConfig().getDouble("checkpoints.beginning.X"),
+                    getConfig().getDouble("checkpoints.beginning.Y"),
+                    getConfig().getDouble("checkpoints.beginning.Z")));
+        }
+    }
+    public void clear() {
+        for (Player p : getServer().getOnlinePlayers()) {
+            queue.remove(p);
+        }
+    }
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+    public void startGame() {
+        if (queue.size() > 2) {
+            timer = getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+                @Override
+                public void run() {
+                    seconds--;
+                    if (seconds != 0) {
+                        getServer().broadcastMessage(ChatColor.GREEN + "" + seconds + " until the game starts");
+                    }
+                    else {
+                        getServer().getScheduler().cancelTask(timer);
+                        seconds = 10;
+                        teleportPlayers();
+                        getServer().broadcastMessage(ChatColor.RED + "GAME STARTED!");
+                    }
+                }
+            }, 20L,20L);
+        }
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player p = (Player) sender;
-        Queue queue = new Queue();
 
         if (cmd.getName().equalsIgnoreCase("startloc")) {
             getConfig().set("checkpoints.beginning.X", p.getLocation().getX());
@@ -58,9 +223,9 @@ public class CodeDay extends JavaPlugin {
         }
 
         if (cmd.getName().equalsIgnoreCase("join")) {
-            if (!queue.isEmpty()) {
-                queue.addToQueue(p);
-                queue.startGame();
+            if (!isEmpty()) {
+                addToQueue(p);
+                startGame();
             }
             else {
                 p.sendMessage(ChatColor.RED + "Game already in progress!");
